076db8266d8863e433e1d563f392e6e443a877f7
Vandana <nagavandana0703@email.com>
2023-12-28 13:49:12 +0530

    sample example

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
 
+
+console.log('-----------testing-------')
+
+const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx)=>{
+    return res(ctx.json(
+      [
+        {id:1,title:'Title1'},
+        {id:2,title:'Title2'},
+        {id:3,title:'Title3'}
+      ]
+    ))
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(()=>{
+  server.listen();
+});
+
+afterAll(()=>{
+  server.close();
+});
+afterEach(()=>{
+  server.resetHandlers();
+});
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Title1/i);
   expect(linkElement).toBeInTheDocument();
 });
